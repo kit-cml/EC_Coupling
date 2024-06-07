f77918ab4c65dd400e0248805dc47c9763e4fbc9
Iga-CML_Linux <dm.adhitama@gmail.com>
2024-06-07 09:12:36 +0900

    add drug effect

--- newmain.cpp
+++ newmain.cpp
@@ -105,7 +105,7 @@ int main(int argc, char **argv)
   // drug initialisation
   drug_t IC50;
   bool is_cai_scaling = true;
-  double conc = 38.0*0;
+  double conc = 38.0;
   double bcl = 1000.;
 
   // cell object pointer
